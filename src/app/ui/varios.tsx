132026b426704c33c8e4f98034974ceb698d1111
fedemoretto11 <fedemoretto94@gmail.com>
2024-03-15 00:00:10 -0300

    fix: Corregido favicon

--- src/app/ui/varios.tsx
+++ src/app/ui/varios.tsx
@@ -1,6 +1,6 @@
 export function BurguerMenuIcon() {
   return (
-    <div className="w-full h-full pointer-events-none flex flex-col items-center justify-center text-inherit group-data-[pressed=true]:opacity-70 transition-opacity before:content-[''] before:block before:h-[2px] before:w-10 before:bg-red-50 before:transition-transform before:duration-150 before:-translate-y-1 before:rotate-0 group-data-[open=true]:before:translate-y-px group-data-[open=true]:before:rotate-45 after:content-[''] after:block after:h-[2px] after:w-10 after:bg-red-50 after:transition-transform after:duration-150 after:translate-y-1 after:rotate-0 group-data-[open=true]:after:translate-y-0 group-data-[open=true]:after:-rotate-45"> 
+    <div className="w-full h-full pointer-events-none flex flex-col items-center justify-center text-inherit group-data-[pressed=true]:opacity-70 transition-opacity before:content-[''] before:block before:h-[2px] before:w-10 before:bg-red-50  before:transition-transform before:duration-150 before:-translate-y-1 before:rotate-0 group-data-[open=true]:before:translate-y-px group-data-[open=true]:before:rotate-45 after:content-[''] after:block after:h-[2px] after:w-10 after:bg-red-50   after:transition-transform after:duration-150 after:translate-y-1 after:rotate-0 group-data-[open=true]:after:translate-y-0 group-data-[open=true]:after:-rotate-45"> 
     </div>
   )
 }
